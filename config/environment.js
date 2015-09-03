e6023326b973d5b7980b89dfe4cf6c71cb96d7f9
BransonGitomeh <sirbranson@outlook.com>
2015-09-03 15:22:04 -0400

    made pods to use the term Modules

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'materialize',
+    podsModulePrefix: 'materialize/Modules',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
