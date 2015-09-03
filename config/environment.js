94fcd700edac5f773985a3c296a934208b7fefa4
BransonGitomeh <sirbranson@outlook.com>
2015-09-03 16:10:54 -0400

    added the matetrialize lib addon...much more sencible than doing it myself

--- config/environment.js
+++ config/environment.js
@@ -3,7 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'materialize',
-    podsModulePrefix: 'materialize/Modules',
+    podModulePrefix: 'materialize/Modules',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
