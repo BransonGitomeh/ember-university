4fb2681070a770a00c8df69de91d171bb7306742
BransonGitomeh <sirbranson@outlook.com>
2015-09-04 02:06:27 -0400

    added base for fixing scroll on navbar

--- app/Modules/components/wrappers/side-nav/component.js
+++ app/Modules/components/wrappers/side-nav/component.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  didInsertElement: function() {
+    // Perfect Scrollbar
+    $('select').not('.disabled').material_select();
+
+    //for normal side-vav
+    var leftnav = $(".page-topbar").height();
+    var leftnavHeight = window.innerHeight - leftnav;
+    $('.leftside-navigation').height(leftnavHeight).perfectScrollbar({
+    suppressScrollX: true
+    });
+
+    //for rightside-navigation for chat
+    var righttnav = $("#chat-out").height();
+    $('.rightside-navigation').height(righttnav).perfectScrollbar({
+    suppressScrollX: true
+    });
+
+ }.on('didInsertElement')
 });
