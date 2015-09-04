b1fc4695b121067ddc8bb650f329f13ff5a40f8f
BransonGitomeh <sirbranson@outlook.com>
2015-09-04 02:38:52 -0400

    finalized navbar, more fixing and reqrite will come later

--- app/Modules/components/wrappers/side-nav/component.js
+++ app/Modules/components/wrappers/side-nav/component.js
@@ -3,13 +3,18 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   didInsertElement: function() {
     // Perfect Scrollbar
-    $('select').not('.disabled').material_select();
+    Ember.$('select').not('.disabled').material_select();
 
     //for normal side-vav
     var leftnav = $(".page-topbar").height();
+
     var leftnavHeight = window.innerHeight - leftnav;
-    $('.leftside-navigation').height(leftnavHeight).perfectScrollbar({
-    suppressScrollX: true
+
+    Ember.$('.leftside-navigation').height(leftnavHeight).perfectScrollbar({
+      //suppressScrollX: true
+      wheelSpeed: 2,
+      wheelPropagation: true,
+      minScrollbarLength: 20
     });
 
 
