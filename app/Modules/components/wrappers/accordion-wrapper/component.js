c1039525a192b6c6ac50012acb107a6a7f1c24f5
BransonGitomeh <sirbranson@outlook.com>
2015-09-04 04:51:11 -0400

    removed somebreaking jquery-ember glue

--- app/Modules/components/wrappers/accordion-wrapper/component.js
+++ app/Modules/components/wrappers/accordion-wrapper/component.js
@@ -1,11 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName="",
-  didInsertElement: function() {
-    Ember.$('.collapsible').collapsible({
-      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-    });
-  }.on('didInsertElement')
+  tagName=" ",
+
+  // didInsertElement: function() {
+  //   Ember.$('.collapsible').collapsible({
+  //     accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+  //   });
+  // }.on('didInsertElement')
+
 
 });
