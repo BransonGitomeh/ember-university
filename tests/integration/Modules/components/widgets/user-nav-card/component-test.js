325eb3e86f4a124f180f281e10fd904e614d3719
BransonGitomeh <sirbranson@outlook.com>
2015-09-17 20:08:06 +0300

    more widgets and clearer layouts path

--- tests/integration/Modules/components/widgets/user-nav-card/component-test.js
+++ tests/integration/Modules/components/widgets/user-nav-card/component-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('widgets/user-nav-card', 'Integration | Component | widgets/user nav card', {
+moduleForComponent('widgets/admin-sidenav-card', 'Integration | Component | widgets/user nav card', {
   integration: true
 });
 
@@ -11,15 +11,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{widgets/user-nav-card}}`);
+  this.render(hbs`{{widgets/admin-sidenav-card}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#widgets/user-nav-card}}
+    {{#widgets/admin-sidenav-card}}
       template block text
-    {{/widgets/user-nav-card}}
+    {{/widgets/admin-sidenav-card}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
