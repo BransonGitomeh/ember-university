ca3c98c96a497e1e5103d486b718a128be29aa42
Gideon Kamau Maina <maina.gidi@gmail.com>
2015-09-18 19:19:48 +0300

    fixed jshint semi colon errors

--- app/router.js
+++ app/router.js
@@ -12,26 +12,47 @@ Router.map(function() {
     //student list views
     this.route('activestudents',function(){
       this.route('details',function(){
-        this.route('profile')
-        this.route('payments')
-        this.route('faults')
-      })
+        this.route('profile');
+        this.route('payments');
+        this.route('faults');
+      });
     });
     this.route('completedstudents',function(){
       this.route('details',function(){
-        this.route('profile')
-        this.route('payments')
-        this.route('faults')
-      })
+        this.route('profile');
+        this.route('payments');
+        this.route('faults');
+      });
     });
     this.route('issuesstudents',function(){
       this.route('details',function(){
-        this.route('profile')
-        this.route('payments')
-        this.route('faults')
-      })
+        this.route('profile');
+        this.route('payments');
+        this.route('faults');
+      });
     });
-
+    //Lecturers details to follow here
+    this.route('fulltimelecturers', function() {
+      this.route('details', function(){
+        this.route('profile');
+        this.route('payments');
+        this.route('history');
+      });
+    });
+    this.route('parttimelecturers', function() {
+      this.route('details', function(){
+        this.route('profile');
+        this.route('payments');
+        this.route('history');
+      });
+    });
+    this.route('promotionallecturers', function() {
+      this.route('details', function(){
+        this.route('profile');
+        this.route('payments');
+        this.route('history');
+      });
+    });
     //data entry forms
     this.route('newStudentForm');
     this.route('newEmployeeForm');
